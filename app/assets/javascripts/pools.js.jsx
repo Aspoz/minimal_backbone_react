a4d071912e15b73fe81b3e030dd046a0ed4f9695
Sander Tuin <github@sandert.me>
2014-04-17 14:13:06 +0200

    Metatag for viewport

--- app/assets/javascripts/pools.js.jsx
+++ app/assets/javascripts/pools.js.jsx
@@ -16,3 +16,24 @@ React.renderComponent(
   <poolsView pools={poolsCollection} />,
   document.getElementById('container')
 );
+
+// var poolsView = React.createBackboneClass({
+//   render: function() {
+//     return <p>Hallo</p>;
+//   }
+// });
+
+// React.renderComponent(
+//   <poolsView />,
+//   document.getElementById('container')
+// );
+
+// React.renderComponent(
+//   React.DOM.h1(null, "Hello, world!"),
+//   document.getElementById('container')
+// );
+
+// React.renderComponent(
+//   <h1>Hello, world!</h1>,
+//   document.getElementById('example')
+// );
